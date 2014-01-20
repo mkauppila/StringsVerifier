d8a9e635ef9ed2cb2f5e020d453ab2f41853b7c1
Markus Kauppila <markus.kauppila@gmail.com>
2014-01-20 17:50:03 +0200

    Print usage if app receives invalid number of arguments

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,17 @@
 class App
   def run arguments
+    if arguments.length != 1
+      print_usage
+      exit
+    end
+
+    filename = arguments.first
+
+    print "filename: #{filename}\n"
   end
+
+  def print_usage
+    print "usage: ./strings-verifier strings-file\n"
   end
 end
 
