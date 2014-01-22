b196d47785e5d5da86e6ee52bb7168f90d73f116
Markus Kauppila <markus.kauppila@gmail.com>
2014-01-22 20:15:54 +0200

    Add some basic verification functionality

--- app/app.rb
+++ app/app.rb
@@ -1,3 +1,26 @@
+class Parser
+  def initialize(file)
+    @file = file 
+  end
+
+  def starts_comment?
+  end
+end
+
+class Verifier
+  def starts_comment(line)
+    line.start_with? "/*"
+  end
+
+  def is_localizable_string(line)
+    line.start_with? '"'
+  end
+
+  def is_empty_line(line)
+    line.length == 0
+  end
+end
+
 class App
   def run arguments
     if arguments.length != 1
@@ -5,9 +28,41 @@ class App
       exit
     end
 
-    filename = arguments.first
+    verifier = Verifier.new
 
-    print "filename: #{filename}\n"
+    filename = arguments.first
+    counter = 1
+    File.open(filename) do |file|
+      file.each do |line|
+
+      line.strip!
+
+=begin 
+      if verifier.starts_comment(line)
+        read the entire comment and go on
+      elsif verifier.is_localizable_string
+        verify the string and go aon
+      elsif on empty line
+         just go on
+      else
+        print the errornous line and give a syntax error
+      end
+=end
+        print "#{counter} #{line}"
+
+
+        if verifier.is_localizable_string(line)
+          print "is a local string\n"
+        elsif verifier.is_empty_line(line)
+          print "is empty line\n"
+        else 
+          print "did recognize: '#{line}'\n"
+        end
+
+
+        counter += 1
+      end
+    end
   end
 
   def print_usage
