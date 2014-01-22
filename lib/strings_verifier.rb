f3f52979c0a831bba5a7b333b3c701d0115b64ac
Markus Kauppila <markus.kauppila@gmail.com>
2014-01-22 21:05:45 +0200

    Clean up Source class

--- lib/strings_verifier.rb
+++ lib/strings_verifier.rb
@@ -22,15 +22,22 @@ class Verifier
 end
 
 class Source
-  def initialize(raw_contents)
-    @lines = raw_contents.split("\n")
+  def initialize(raw_contents_of_file)
+    @lines = split_file_contents_to_lines(raw_contents_of_file)
     @current_line = 0
   end
 
+  
   def get_line
     line = @lines[@current_line]
     @current_line += 1
     line
+  end
+
+  private
+  
+  def split_file_contents_to_lines(file_contents)
+    file_contents.split("\n")
   end
 end
 
