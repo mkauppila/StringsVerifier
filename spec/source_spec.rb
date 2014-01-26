6d5834c36dae53fa323486ae72553e7480782643
Markus Kauppila <markus.kauppila@gmail.com>
2014-01-26 19:23:04 +0200

    add specs for source

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,6 +1,22 @@
 require 'spec_helper'
 
 describe Source do 
-  it "should do nothing" do
+  before do
+    contents = "first\nsecond\nthird\n"
+    @source = Source.new(contents)
+  end
+
+  it "should be created" do
+    @source.must_be_kind_of Source
+  end
+
+  it "should have three lines" do
+    @source.number_of_lines.must_equal 3
+  end
+
+  it "it should advance lines" do
+    @source.get_line.must_match /first/
+    @source.get_line.must_match /second/
+    @source.get_line.must_match /third/
   end
 end
