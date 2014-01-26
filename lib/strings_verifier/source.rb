6afb2524006193fb8f89be1a15e21440628a8d27
Markus Kauppila <markus.kauppila@gmail.com>
2014-01-26 19:22:31 +0200

    Add source.rb
    Contains the Source class.

---
+++ lib/strings_verifier/source.rb
@@ -0,0 +1,22 @@
+class Source
+  def initialize(raw_contents_of_file)
+    @lines = split_file_contents_to_lines(raw_contents_of_file)
+    @current_line = 0
+  end
+
+  def get_line
+    line = @lines[@current_line]
+    @current_line += 1
+    line
+  end
+
+  def number_of_lines
+    @lines.size
+  end
+
+  private
+
+  def split_file_contents_to_lines(file_contents)
+    file_contents.split("\n")
+  end
+end
