dbcef7b4ef8afd5791ced35dcecf3a7bb49cc90c
Markus Kauppila <markus.kauppila@gmail.com>
2014-01-28 21:25:58 +0200

    Clean up dead code from lib/strings_verifier.rb

--- lib/strings_verifier.rb
+++ lib/strings_verifier.rb
@@ -1,85 +1,23 @@
 require 'strings_verifier/source'
 require 'strings_verifier/version'
 
-class Verifier
-  def starts_comment(line)
-    line.start_with? "/*"
-  end
-
-  def is_localizable_string(line)
-    line.start_with? '"'
-  end
-
-  def is_empty_line(line)
-    line.length == 0
-  end
-end
-
-
-
-
-class StringsVerifierA
-  def run arguments
-    if arguments.length != 1
-      print_usage
-      return
-    end
- 
-    line_counter = 1
-    raw_contents = File.read(arguments.first)
-    source = Source.new(raw_contents)
-    loop do
-      line = source.get_line
-      if line.nil?
-        break
-      end
-      print "#{line_counter} '#{line}'\n"
-
-      line_counter += 1
+module StringsVerifier
+  class Verifier
+    def starts_comment(line)
+      line.start_with? "/*"
     end
 
-    return 
-
-
-    
-
-    verifier = Verifier.new
-
-    filename = arguments.first
-    counter = 1
-    File.open(filename) do |file|
-      file.each do |line|
-
-        line.strip!
-
-=begin 
-      if verifier.starts_comment(line)
-        read the entire comment and go on
-      elsif verifier.is_localizable_string
-        verify the string and go aon
-      elsif on empty line
-         just go on
-      else
-        print the errornous line and give a syntax error
-      end
-=end
-        print "#{counter} #{line}"
-
-        if verifier.is_localizable_string(line)
-          print "is a local string\n"
-        elsif verifier.is_empty_line(line)
-          print "is empty line\n"
-        else 
-          print "did recognize: '#{line}'\n"
-        end
-
-
-        counter += 1
-      end
+    def is_localizable_string(line)
+      line.start_with? '"'
     end
-  end
 
-  def print_usage
-    print "usage: ./strings-verifier strings-file\n"
+    def is_empty_line(line)
+      line.length == 0
+    end
+
+    def process(source)
+      # run through the source, 
+      # collect errors to list and return it
+    end
   end
 end
