b29f6891bd98493d77e59cdacf7f2e7819f9834c
Markus Kauppila <markus.kauppila@gmail.com>
2014-01-29 20:06:16 +0200

    Simply Verifier spec
    Delete the inner describes and make it easier for eyes.

--- spec/verifier_spec.rb
+++ spec/verifier_spec.rb
@@ -5,37 +5,31 @@ describe "Verifier should recognize different types of data" do
     @verifier = StringsVerifier::Verifier.new
   end
 
-  describe "should recognize an empty line" do
-    it "should recognize '' as empty line" do 
-      @verifier.is_empty_line?('').must_be  :==, true
-    end
-
-    it "should recognize '  ' as empty line" do 
-      @verifier.is_empty_line?('  ').must_be  :==, true
-    end
-
-    it "should recognize ' \n  ' as empty line" do 
-      @verifier.is_empty_line?(" \n  ").must_be  :==, true
-    end
+  it "should recognize '' as empty line" do 
+    @verifier.is_empty_line?('').must_be  :==, true
   end
 
-  describe "should recognize a line that starts a comment" do
-    it "should recognize '/*' as comment " do
-      @verifier.starts_comment?("/*").must_be :==, true
-    end
-
-    it "should recognize ' /* ' as comment " do
-      @verifier.starts_comment?(" /* ").must_be :==, true
-    end
+  it "should recognize '  ' as empty line" do 
+    @verifier.is_empty_line?('  ').must_be  :==, true
   end
 
-  describe "should recognize a line containing localizable string" do
-    it "should recognize '\"' "  do 
-      @verifier.is_localizable_string?('"').must_be :==, true
-    end
+  it "should recognize ' \n  ' as empty line" do 
+    @verifier.is_empty_line?(" \n  ").must_be  :==, true
+  end
 
-    it "should recognize ' \" ' "  do 
-      @verifier.is_localizable_string?(' " ').must_be :==, true
-    end
+  it "should recognize '/*' as comment " do
+    @verifier.starts_comment?("/*").must_be :==, true
+  end
+
+  it "should recognize ' /* ' as comment " do
+    @verifier.starts_comment?(" /* ").must_be :==, true
+  end
+
+  it "should recognize '\"' as localizable string"  do 
+    @verifier.is_localizable_string?('"').must_be :==, true
+  end
+
+  it "should recognize ' \" ' as localizable string"  do 
+    @verifier.is_localizable_string?(' " ').must_be :==, true
   end
 end
