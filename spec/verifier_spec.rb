f73cc3a0b3866057f91674b2737ec8b2c316add3
Markus Kauppila <markus.kauppila@gmail.com>
2014-01-29 07:11:54 +0200

    Fleshing out the Verifier spec

--- spec/verifier_spec.rb
+++ spec/verifier_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe StringsVerifier::Verifier do 
+  before do
+    @verifier = StringsVerifier::Verifier.new
+  end
 
+  it "should recognize '' as empty line" do 
+    @verifier.is_empty_line?('').must_be  :==, true
+  end
+
+  it "should recognize '  ' as empty line" do 
+    @verifier.is_empty_line?('  ').must_be  :==, true
+  end
+
+  it "should recognize ' \n  ' as empty line" do 
+    @verifier.is_empty_line?(" \n  ").must_be  :==, true
+  end
 end
