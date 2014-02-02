3655c978db2a135f81549306d9efbf25c4c82ba1
Markus Kauppila <markus.kauppila@gmail.com>
2014-02-02 20:13:06 +0200

    Refactor Source spec
    Use let instead of instance variable.

--- spec/source_spec.rb
+++ spec/source_spec.rb
@@ -1,26 +1,26 @@
 require 'spec_helper'
 
 describe StringsVerifier::Source do 
-  before do
+  let(:source) do
     contents = "first\nsecond\nthird\n"
-    @source = StringsVerifier::Source.new(contents)
+    StringsVerifier::Source.new(contents)
   end
 
   it "should be created" do
-    @source.must_be_kind_of StringsVerifier::Source
+    source.must_be_kind_of StringsVerifier::Source
   end
 
   it "should have three lines" do
-    @source.number_of_lines.must_equal 3
+    source.number_of_lines.must_equal 3
   end
 
   it "should return the current line" do
-    @source.get_current_line.must_match /first/
+    source.get_current_line.must_match /first/
   end
 
   it "should be able to advance the line" do
-    @source.get_current_line.must_match /first/
-    @source.advance_to_next_line
-    @source.get_current_line.must_match /second/
+    source.get_current_line.must_match /first/
+    source.advance_to_next_line
+    source.get_current_line.must_match /second/
   end
 end
